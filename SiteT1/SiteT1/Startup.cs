8e7bff11c0cc725f98388f2dfe175b1502ba23d5
MrGoIdy <zolotarev.me@mail.ru>
2017-02-02 23:25:45 +0300

    C#Site

---
+++ SiteT1/SiteT1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteT1.Startup))]
+namespace SiteT1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
